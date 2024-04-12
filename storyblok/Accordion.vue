c97d100c70a04465ce4ed0296fc7291ec1f905f6
vester6 <vester6@gmail.com>
2024-04-12 13:52:30 +0200

    accordionlist

---
+++ storyblok/Accordion.vue
@@ -0,0 +1,77 @@
+<template>
+  <div v-editable="accordion" class="accordion">
+    <div class="accordion-header" @click="toggleAccordion">
+      <h3>{{ accordion.headline }}</h3>
+      <span class="accordion-icon">{{ isExpanded ? "-" : "+" }}</span>
+    </div>
+    <div
+      class="accordion-content"
+      :style="{ display: isExpanded ? 'block' : 'none' }"
+    >
+      <img
+        v-if="accordion.image"
+        :src="accordion.image.filename"
+        :alt="accordion.image.alt"
+      />
+      <p>{{ accordion.bodytext }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+defineProps({ accordion: Object });
+
+const isExpanded = ref(false);
+
+const toggleAccordion = () => {
+  isExpanded.value = !isExpanded.value;
+};
+</script>
+
+<style scoped>
+.accordion {
+  border-bottom: 1px solid #e1e1e1;
+  transition: background-color 0.3s ease;
+}
+
+.accordion:last-child {
+  border-bottom: none;
+}
+
+.accordion-header {
+  padding: 30px 30px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: background-color 0.3s ease;
+}
+
+.accordion-header:hover {
+  background-color: #e2f5fc;
+}
+
+.accordion-icon {
+  font-size: 24px;
+  line-height: 0; /* Adjusts line height to match the icon size */
+}
+
+.accordion-content {
+  padding: 20px;
+  background-color: white;
+  transition: all 0.3s ease-in-out;
+}
+
+.accordion-content img {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 15px;
+}
+
+.accordion-content p {
+  margin: 0;
+  font-size: 16px;
+  line-height: 1.5;
+}
+</style>
---
+++ storyblok/AccordionList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-editable="blok" class="accordion-list">
+    <Accordion
+      v-for="accordion in blok.accordions"
+      :key="accordion._uid"
+      :accordion="accordion"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<style scoped>
+.accordion-list {
+  max-width: 984px;
+  margin: 20px auto;
+  border: 1px solid #ced4da; /* Adds a light border around the whole list */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
