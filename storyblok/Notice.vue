33f265d8cdccd67fc4d11d649a604431862c2176
vester6 <vester6@gmail.com>
2024-04-15 13:33:22 +0200

    notice

---
+++ storyblok/Notice.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-editable="blok" class="container">
+    <div class="content">
+      <img :src="blok.icon.filename" :alt="blok.icon.alt" class="icon" />
+      <p class="text">{{ blok.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({ blok: Object });
+</script>
+
+<style scoped>
+.container {
+  background-color: #effafe;
+  color: #0c0931;
+  display: flex;
+  flex-direction: column;
+  align-items: left; /* Ensure the container is centered */
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 24px;
+  max-width: 646px;
+  margin: auto; /* Centering in the viewport */
+  padding: 30px;
+  /* Take full viewport height */
+}
+
+@media (max-width: 991px) {
+  .container {
+    padding: 20px 20px;
+  }
+}
+
+.content {
+  display: flex;
+  gap: 15px;
+  align-items: center; /* Align items vertically */
+}
+
+@media (max-width: 991px) {
+  .content {
+    flex-direction: column;
+  }
+}
+
+.icon {
+  align-self: start;
+  object-fit: contain;
+  width: 21px;
+  height: 21px; /* Match the width if aspect ratio is to be maintained */
+}
+
+.text {
+  font-family: "Open Sans", sans-serif;
+}
+
+@media (max-width: 991px) {
+  .text {
+    max-width: 100%;
+  }
+}
+</style>
---
+++ storyblok/NumberedList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="centered-container">
+    <div v-editable="blok" class="container">
+      <div class="circle">
+        <p class="number">{{ blok.number }}</p>
+      </div>
+      <div class="divider"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({ blok: Object });
+</script>
+
+<style scoped>
+.centered-container {
+  display: flex;
+  justify-content: center; /* Center horizontally */
+  align-items: center; /* Center vertically */
+  height: 10vh; /* Full viewport height */
+}
+.number {
+  /* Center the number both vertically and horizontally */
+  display: flex;
+  align-items: center;
+  font-family: "Open Sans";
+  font-weight: 500;
+  justify-content: center;
+  margin: 0; /* Reset margin to remove any default spacing */
+  height: 100%; /* Ensure the number takes up the full height of the circle */
+}
+.container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 984px;
+  width: 100%; /* Ensure it takes the available space */
+}
+
+.circle {
+  position: absolute; /* Position it over the divider */
+  top: 50%; /* Center vertically */
+  transform: translateY(
+    -50%
+  ); /* Offset the height of the circle for true centering */
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #e2f5fc;
+  z-index: 1; /* Ensures it sits above the divider */
+}
+
+.divider {
+  position: absolute;
+  width: 100%; /* Full width of the container */
+  top: 50%; /* Align with the middle of the circle */
+  border-top: 1px solid #e2f5fc; /* 1px thick, color as specified */
+}
+</style>
