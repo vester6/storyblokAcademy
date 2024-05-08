be7a687de643387ae7c313ff689701ad91774b5b
vester6 <vester6@gmail.com>
2024-05-08 13:09:29 +0200

    fixes

--- storyblok/LabeledGraphic.vue
+++ storyblok/LabeledGraphic.vue
@@ -1,7 +1,11 @@
 <template>
   <div v-editable="blok" class="labeled-graphic" @click="closePopup()">
     <h2 class="labeled-graphic__headline">{{ blok.headline }}</h2>
-    <p class="labeled-graphic__bodytext">{{ blok.bodyText }}</p>
+    <div
+      class="labeled-graphic__bodytext"
+      v-html="labeledgraphicbodytext"
+    ></div>
+
     <div class="image-container">
       <img
         :src="blok.image.filename"
@@ -17,103 +21,189 @@
       >
         <div class="marker-icon">
           <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
+            <circle cx="15" cy="15" r="15" fill="#2084C9" />
             <path
-              d="M10.44 5.005H6.82V1.4C6.82 0.905 6.415 0.5 5.92 0.5C5.425 0.5 5.02 0.905 5.02 1.4V5.02H1.4C0.905 5.02 0.5 5.425 0.5 5.92C0.5 6.415 0.905 6.82 1.4 6.82H5.02V10.44C5.02 10.935 5.425 11.34 5.92 11.34C6.415 11.34 6.82 10.935 6.82 10.44V6.805H10.44C10.935 6.805 11.34 6.4 11.34 5.905C11.34 5.41 10.935 5.005 10.44 5.005Z"
+              d="M19.44 14.005H15.82V10.4C15.82 9.905 15.415 9.5 14.92 9.5C14.425 9.5 14.02 9.905 14.02 10.4V14.02H10.4C9.905 14.02 9.5 14.425 9.5 14.92C9.5 15.415 9.905 15.82 10.4 15.82H14.02V19.44C14.02 19.935 14.425 20.34 14.92 20.34C15.415 20.34 15.82 19.935 15.82 19.44V15.805H19.44C19.935 15.805 20.34 15.4 20.34 14.905C20.34 14.41 19.935 14.005 19.44 14.005Z"
               fill="white"
             />
           </svg>
-          <div class="pulse"></div>
+          <div
+            class="pulse"
+            :style="{ animationDelay: marker.animationDelay + 's' }"
+          ></div>
         </div>
       </div>
     </div>
-    <div v-if="currentMarker" class="popup" :style="popupStyle">
+
+    <!-- Modal for smaller screens -->
+    <div v-if="isModalActive && isMobile" class="modal">
+      <div class="modal-content">
+        <span class="close" @click="closePopup()">&times;</span>
+        <img
+          v-if="currentMarker.popupImage && currentMarker.popupImage.filename"
+          :src="currentMarker.popupImage.filename"
+          class="popupimage"
+          alt="Marker Detail"
+        />
+        <h3>{{ currentMarker.popupText }}</h3>
+        <div class="labeled-graphic__bodytext" v-html="popupbodytext"></div>
+      </div>
+    </div>
+
+    <!-- Popup for larger screens -->
+    <div
+      v-if="currentMarker && !isMobile"
+      class="popup"
+      :style="popupStyle"
+      :class="{ 'popup-visible': currentMarker }"
+    >
       <img
+        v-if="currentMarker.popupImage && currentMarker.popupImage.filename"
         :src="currentMarker.popupImage.filename"
         class="popupimage"
         alt="Marker Detail"
       />
       <h3>{{ currentMarker.popupText }}</h3>
-      <p class="bodytextpopup">{{ currentMarker.popupbodytext }}</p>
+
+      <div class="labeled-graphic__bodytext" v-html="popupbodytext"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, onMounted } from "vue";
-
+import { ref, computed, defineProps } from "vue";
 const props = defineProps({
   blok: Object,
 });
+const labeledgraphicbodytext = computed(() =>
+  renderRichText(props.blok.bodyText)
+);
+const popupbodytext = computed(() => {
+  if (currentMarker.value && currentMarker.value.popupbodytext) {
+    return renderRichText(currentMarker.value.popupbodytext);
+  }
+  return "";
+});
+
 const currentMarker = ref(null);
 const popupStyle = ref({});
+const isModalActive = ref(false);
+
+// Compute random animation delays for each marker
+onMounted(() => {
+  props.blok.markers.forEach((marker) => {
+    // Assign a random delay from 0 to the duration of the animation cycle (2 seconds here)
+    marker.animationDelay = Math.random() * 2; // Random delay between 0 and 2 seconds
+  });
+});
+
+const isMobile = computed(() => {
+  return window.innerWidth < 768; // Adjust threshold as needed
+});
 
 function showPopup(marker, event) {
   currentMarker.value = marker;
-  const markerElem = event.target;
-  const imageContainer = markerElem.closest(".image-container");
-  const imageRect = imageContainer.getBoundingClientRect();
+  isModalActive.value = true; // Activate modal on marker click if on mobile
+  const imageContainer = document.querySelector(".image-container");
 
-  const markerPosX = (marker.x / 100) * imageRect.width;
-  const markerPosY = (marker.y / 40) * imageRect.height;
+  if (!imageContainer) return;
 
-  // Determines if the popup should display to the right or left based on marker position
-  const shouldDisplayRight = marker.x < 50;
+  const containerTop = imageContainer.offsetTop;
+  const containerLeft = imageContainer.offsetLeft;
+
+  const markerPosX = (marker.x / 100) * imageContainer.offsetWidth;
+  const markerPosY = (marker.y / 200) * imageContainer.offsetHeight;
+
+  // Determine which side of the marker the popup should appear on
+  const shouldDisplayLeft = marker.x >= 50; // Popups for markers on the right side appear to the left
 
   popupStyle.value = {
-    top: markerPosY + imageRect.top + "px",
-    left: shouldDisplayRight ? markerPosX + imageRect.left + 15 + "px" : "auto",
-    right: shouldDisplayRight
+    top: markerPosY + containerTop + "px",
+    left: shouldDisplayLeft ? markerPosX + containerLeft - 370 + "px" : "auto", // Position left if marker is on the right
+    right: shouldDisplayLeft
       ? "auto"
       : document.documentElement.clientWidth -
         markerPosX -
-        imageRect.left +
-        15 +
-        "px",
+        containerLeft -
+        400 +
+        "px", // Position right if marker is on the left
+    visibility: "visible",
+    opacity: 1,
   };
 }
 
 function closePopup() {
-  currentMarker.value = null;
+  if (currentMarker.value) {
+    popupStyle.value = { ...popupStyle.value, opacity: 0 };
+    isModalActive.value = false;
+    setTimeout(() => {
+      currentMarker.value = null;
+    }, 300);
+  }
 }
 </script>
 
 <style scoped>
+.labeled-graphic__headline {
+  padding-bottom: 30px;
+}
+.labeled-graphic__bodytext {
+  padding-bottom: 30px;
+  font-family: "Open Sans";
+  font-size: 16px;
+  line-height: 22px;
+  font-weight: 300;
+}
 .labeled-graphic {
   max-width: 984px;
   margin: 0 auto;
+  margin-top: 60px;
 }
-
 .image-container {
   position: relative;
   cursor: pointer;
   text-align: center;
   width: 100%;
 }
-
 .labeled-graphic__image {
   width: 100%;
   height: auto;
 }
-.popupimage {
-  width: 100%;
-  height: auto;
-  margin-bottom: 10px;
-}
-
 .bodytextpopup {
-  padding-top: 10px;
+  font-family: "Open Sans";
   font-size: 16px;
   line-height: 22px;
-  font-family: Oscine, sans-serif;
+  font-weight: 300;
+  padding-top: 10px;
   color: #0c0931;
 }
+.popup {
+  position: absolute;
+  background: rgba(255, 255, 255, 0.9);
+  backdrop-filter: blur(15px);
+  padding: 15px;
+  border-radius: 6px;
+  z-index: 1000;
+  width: 350px;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  visibility: hidden;
+}
 
+.popupimage {
+  width: 100%;
+  padding-bottom: 15px;
+}
+.popup-visible {
+  opacity: 1;
+  visibility: visible;
+}
 .marker {
   position: absolute;
   cursor: pointer;
@@ -126,37 +216,60 @@ function closePopup() {
   align-items: center;
   justify-content: center;
 }
-
 .pulse {
   position: absolute;
   width: 30px;
   height: 30px;
   border-radius: 50%;
-  background-color: rgba(114, 199, 231, 0.7);
+  background-color: rgba(114, 199, 231, 0.85);
   animation: pulse-animation 2s infinite;
 }
-
 @keyframes pulse-animation {
   0% {
-    transform: scale(1.2);
+    transform: scale(1);
     opacity: 1;
   }
-  70% {
-    transform: scale(2.5);
-    opacity: 0, 5;
-  }
+
   100% {
     transform: scale(3.5);
     opacity: 0;
   }
 }
-
-.popup {
-  position: absolute;
+.modal {
+  display: flex;
+  position: fixed;
+  z-index: 1001;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: #00308733;
+}
+.modal-content {
+  margin: auto;
   background: white;
-  border: 1px solid #ccc;
-  padding: 20px;
-  z-index: 1000;
-  max-width: 250px;
+  padding: 15px;
+  border-radius: 6px;
+  max-width: 90%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+@media (min-width: 768px) {
+  .modal,
+  .modal-content {
+    display: none;
+  }
 }
 </style>
